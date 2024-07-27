bbaad8c4171905f3c80901c72ad5b16b65c0fff0
Russell Kerber <russell.kerber@gmail.com>
2024-07-27 13:57:24 -0600

    changed app.json to app.config.js

--- app.config.js
+++ app.config.js
@@ -1,4 +1,4 @@
-{
+module.exports = {
   "expo": {
     "name": "vibecheq",
     "slug": "vibecheq",
@@ -15,12 +15,12 @@
       "**/*"
     ],
     "ios": {
-      "googleServicesFile": "./GoogleService-Info.plist",
+      "googleServicesFile": process.env.GOOGLE_SERVICE_INFO_PLIST || "./GoogleService-Info.plist",
       "supportsTablet": true,
       "bundleIdentifier": "com.vibecheq.dev"
     },
     "android": {
-      "googleServicesFile": "google-services.json",
+      "googleServicesFile": process.env.GOOGLE_SERVICES_JSON || "google-services.json",
       "adaptiveIcon": {
         "foregroundImage": "./assets/adaptive-icon.png",
         "backgroundColor": "#ffffff"
@@ -61,6 +61,11 @@
           "iosAppId": "ca-app-pub-9408101332805838~3498696922"
         }
       ]
-    ]
+    ],
+    "extra": {
+      "eas": {
+        "projectId": "18fa69b1-f495-4899-9793-d327be69e418"
+      }
+    }
   }
 }
