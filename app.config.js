bf7242ace07916eee08d858036a60412646360ea
Russell Kerber <russell.kerber@gmail.com>
2025-02-28 17:45:54 -0700

    incremented version

--- app.config.js
+++ app.config.js
@@ -2,7 +2,7 @@ export default {
   "expo": {
     "name": "Vibecheq",
     "slug": "vibecheq",
-    "version": "1.0.11",
+    "version": "1.0.13",
     "orientation": "portrait",
     "icon": "./assets/icon.png",
     "userInterfaceStyle": "light",
